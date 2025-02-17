dca46dab6d98476dad64b7c6780bf827ca397ea3
umar khitab <umair123123ss@gmail.com>
2025-02-17 12:47:38 +0500

    refactor: update multer storage destination to public directory and remove unused temp directory

--- backend/src/middleWare/multer.middle.js
+++ backend/src/middleWare/multer.middle.js
@@ -3,7 +3,7 @@ const storage = multer.diskStorage(
     {
         
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, "./public")
     },
     filename: function (req, file, cb) {
       
