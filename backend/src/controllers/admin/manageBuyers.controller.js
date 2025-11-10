2d29ecffe97de82d793ba61d14c415795c4ba51d
umar khitab <umair123123ss@gmail.com>
2025-11-11 00:01:13 +0500

    fix: Correct import path casing for User, ApiError, and ApiResponse in manageBuyers.controller

--- backend/src/controllers/admin/manageBuyers.controller.js
+++ backend/src/controllers/admin/manageBuyers.controller.js
@@ -1,6 +1,6 @@
-import { User } from "../../models/user.model.js";
-import { ApiError } from "../../utils/ApiError.js";
-import { ApiResponse } from "../../utils/ApiResponse.js";
+import { User } from "../../models/User.model.js";
+import { ApiError } from "../../utils/apiError.js";
+import { ApiResponse } from "../../utils/apiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
 // Get all buyers
